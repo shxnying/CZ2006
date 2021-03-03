c4156fefd7df8b046af4a492d772d63119416dc1
unknown <jonathanc1699@gmail.com>
2021-03-03 20:17:20 +0800

    login, register and mapview added

---
+++ app/src/main/java/com/example/loginapp/GoogleMap.java
@@ -0,0 +1,89 @@
+package com.example.loginapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+
+import com.google.android.gms.internal.location.zzz;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+public class GoogleMap extends AppCompatActivity {
+    //Initialize variable
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_google_map);
+
+        //assign variable
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        //initialize fused location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        // Check permission
+        if(ActivityCompat.checkSelfPermission(GoogleMap.this, Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            //when permission granted
+            //Call method
+            getCurrentLocation();
+
+        }else{
+            //When permission denied
+            //Request permission
+            ActivityCompat.requestPermissions(GoogleMap.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
+        }
+    }
+
+    private void getCurrentLocation() {
+        //initialize task location
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //When success
+                if(location != null){
+                    //Sync map
+                    supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(com.google.android.gms.maps.GoogleMap googleMap) {
+                            //Initalize Lat Lng
+                            LatLng latlng = new LatLng(location.getLatitude(),location.getLongitude());
+                            //Create marker options
+                            MarkerOptions options = new MarkerOptions().position(latlng).title("I am here");
+                            //Zoom map
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng,10));
+                            //add marker on map
+                            googleMap.addMarker(options);
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode==44){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                //When permission granted
+                //Call method
+                getCurrentLocation();
+            }
+        }
+    }
+}
