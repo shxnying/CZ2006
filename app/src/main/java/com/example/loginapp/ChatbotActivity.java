1a8abb503295e5f30fc040a9c125eeed2ac8a5f0
notlynn002 <70563374+notlynn002@users.noreply.github.com>
2021-03-10 18:11:05 +0800

    Update ChatbotActivity.java

--- app/src/main/java/com/example/loginapp/ChatbotActivity.java
+++ app/src/main/java/com/example/loginapp/ChatbotActivity.java
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
-import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -17,52 +16,65 @@ import java.util.List;
 
 public class ChatbotActivity extends AppCompatActivity {
 
-    private EditText userInput;
-    private RecyclerView recyclerView;
-    @SuppressLint("WrongViewCast")
+    EditText userInput;
+    RecyclerView recyclerView;
+    List<ResponseMessage> responseMessageList;
+    MessageAdapter messageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatbot);
-
         userInput = findViewById(R.id.userInput);
-        userInput.setOnKeyListener(new View.OnKeyListener() {
+        recyclerView = findViewById(R.id.conversation);
+        responseMessageList = new ArrayList<>();
+        messageAdapter = new MessageAdapter(responseMessageList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setAdapter(messageAdapter);
+        userInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    String userInputStr = userInput.getText().toString();
-                    Log.d("info", userInputStr);
-                    userInput.getText().clear();
-                    return true;
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if(actionId == EditorInfo.IME_ACTION_SEND){
+                    ResponseMessage userMessage = new ResponseMessage(userInput.getText().toString(), true);
+                    responseMessageList.add(userMessage);
 
-
+                    //add in if else for chatbot's side of the response message, if user enters stop....else...
+/*                    if (userInput.getText().toString() == "stop"){
+                        ResponseMessage chatbotMessage = new ResponseMessage("i havent done this part yet", false);
+                        responseMessageList.add(chatbotMessage);
+                    }
+                    else {
+                        ArrayList tempList = new ArrayList();
+                        for (int i = 1; i < responseMessageList.size(); i=+2)
+                        {
+                            tempList.add(responseMessageList.get(i));
+                        }
+                        if (tempList.size() == 1) {
+                            ResponseMessage chatbotMessage = new ResponseMessage("Your symptoms are: " + userInput.getText().toString() + ". Enter 'stop' if you do not have anymore symptoms to add", false);
+                            responseMessageList.add(chatbotMessage);
+                        }
+                        else {
+                            ResponseMessage chatbotMessage = new ResponseMessage("Your symptoms are: " + tempList + ". Enter 'stop' if you do not have anymore symptoms to add", false);
+                            responseMessageList.add(chatbotMessage);
+                        }
+                    }*/
+                    ResponseMessage chatbotMessage = new ResponseMessage("Your symptoms are: " + userInput.getText().toString() + ". Enter 'stop' if you do not have anymore symptoms to add", false);
+                    responseMessageList.add(chatbotMessage);
+                    messageAdapter.notifyDataSetChanged();
+                    if(!isVisible()){
+                        recyclerView.scrollToPosition(messageAdapter.getItemCount()-1);
+                    }
 
                 }
-                return false;
+                return true;
             }
         });
-
-
-        recyclerView = findViewById(R.id.recyclerView);
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-
-
-        List<Chatbot> chatbotList = new ArrayList<>();
-        chatbotList.add(new Chatbot("chatbot message 1", "chatbot message 1"));
-        chatbotList.add(new Chatbot("chatbot message 2", "chatbot message 2"));
-        chatbotList.add(new Chatbot("chatbot message 3", "chatbot message 3"));
-        chatbotList.add(new Chatbot("chatbot message 4", "chatbot message 4"));
-        chatbotList.add(new Chatbot("chatbot message 5", "chatbot message 5"));
-        chatbotList.add(new Chatbot("chatbot message 6", "chatbot message 6"));
-        chatbotList.add(new Chatbot("chatbot message 7", "chatbot message 7"));
-
-        Adapter adapter = new Adapter(chatbotList);
-        recyclerView.setAdapter(adapter);
-
-        adapter.notifyDataSetChanged();
-
-        }
     }
+
+
+    public boolean isVisible(){
+        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+        int positionOfLastVisibleItem = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+        int itemCount = recyclerView.getAdapter().getItemCount();
+        return (positionOfLastVisibleItem>=itemCount);
+    }
+}
