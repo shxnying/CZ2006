82af81a106bc52c4d4f8cb00a62f71f3b3606e6c
notlynn002 <70563374+notlynn002@users.noreply.github.com>
2021-03-10 18:10:14 +0800

    Update and rename Adapter.java to MessageAdapter.java

---
+++ app/src/main/java/com/example/loginapp/MessageAdapter.java
@@ -0,0 +1,51 @@
+package com.example.loginapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageAdapter.CustomViewHolder> {
+
+    class CustomViewHolder extends RecyclerView.ViewHolder{
+        TextView textView;
+        public CustomViewHolder(View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.textMessage);
+        }
+    }
+
+    List<ResponseMessage> responseMessageList;
+
+    public MessageAdapter(List<ResponseMessage> responseMessageList) {
+        this.responseMessageList = responseMessageList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (responseMessageList.get(position).isUser()) {
+            return R.layout.user_bubble;
+        }
+        return R.layout.chatbot_bubble;
+    }
+    @NonNull
+    @Override
+    public CustomViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new CustomViewHolder(LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageAdapter.CustomViewHolder holder, int position) {
+        holder.textView.setText(responseMessageList.get(position).getTextMessage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return responseMessageList.size();
+    }
+}
