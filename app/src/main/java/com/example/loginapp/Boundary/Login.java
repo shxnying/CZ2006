d0d89c71536dce7f77fa96776183a2da358d077b
Russellleung <35394164+Russellleung@users.noreply.github.com>
2021-03-24 15:54:34 +0800

    Clinic_admin_page UI is done

--- app/src/main/java/com/example/loginapp/Boundary/Login.java
+++ app/src/main/java/com/example/loginapp/Boundary/Login.java
@@ -104,15 +104,6 @@ public class Login extends AppCompatActivity {
                                         Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
 
 
-
-
-
-
-
-
-
-
-
                                         startActivity(new Intent(getApplicationContext(), MainActivity.class));
 
 
