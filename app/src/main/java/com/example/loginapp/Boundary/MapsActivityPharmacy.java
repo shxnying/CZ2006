916bbbb27ed7cd00077d64822d68986aa4d53a60
jonathanc1699 <70562873+jonathanc1699@users.noreply.github.com>
2021-04-01 10:56:39 +0800

    minor visual updates

--- app/src/main/java/com/example/loginapp/Boundary/MapsActivityPharmacy.java
+++ app/src/main/java/com/example/loginapp/Boundary/MapsActivityPharmacy.java
@@ -183,7 +183,8 @@ public class MapsActivityPharmacy extends AppCompatActivity implements OnMapRead
             if (location != null) {
                 LatLng myLatLng = new LatLng(location.getLatitude(), location.getLongitude());
                 Log.d("tag", "My location is " + location);
-                mMap.addMarker(new MarkerOptions().position(myLatLng).title("You are here"));
+                Marker m = mMap.addMarker(new MarkerOptions().position(myLatLng).title("You are here"));
+                m.showInfoWindow();
 
             }
         });}
