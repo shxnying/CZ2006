8844f7b9c6681f0564fc1d6db6e8c40081472830
Lynn <lynn.masillamoni@gmail.com>
2021-04-02 21:13:03 +0800

    fixed symptom match rate logic

--- app/src/main/java/com/example/loginapp/Control/ChatbotController.java
+++ app/src/main/java/com/example/loginapp/Control/ChatbotController.java
@@ -1,6 +1,8 @@
 package com.example.loginapp.Control;
 
 
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -50,24 +52,34 @@ public class ChatbotController {
         }
 
 
-        int possibleSymptomsCount = 0;
+        int possibleSymptomsCount = 15;
+        int tempPossibleSymptomsCount = 0;
+        List<Integer> symptomsForPossibleDiseases = new ArrayList<>();
         for (Map.Entry<String, ArrayList<String>> entry : myMap.entrySet()) {
             String diseasename2 = entry.getKey();
             for (int s = 0; s < highestcountdiseasearray.size(); s++) {
                 if (diseasename2.equals(highestcountdiseasearray.get(s))) {
-                    ArrayList<String> highestcountdiseasesymptoms = new ArrayList<String>();
-                    ArrayList<String> arraysymptoms = entry.getValue();
-                    possibleSymptomsCount = 0;
-                    for (int t = 0; t < arraysymptoms.size(); t++) {
+                    //ArrayList<String> highestcountdiseasesymptoms = new ArrayList<String>();
+                    ArrayList<String> arraysymptoms = entry.getValue(); //arraylist of all the possible symptoms for diseasename2
 
+
+                    for (int t = 0; t < arraysymptoms.size(); t++) {
                         if (arraysymptoms.get(t).equals("fatigue") || arraysymptoms.get(t).equals("nausea") || arraysymptoms.get(t).equals("swollen glands") || arraysymptoms.get(t).equals("rash") || arraysymptoms.get(t).equals("headache") || arraysymptoms.get(t).equals("abdominal pain") || arraysymptoms.get(t).equals("appetite loss") || arraysymptoms.get(t).equals("fever") || arraysymptoms.get(t).equals("dark urine") || arraysymptoms.get(t).equals("joint pain") || arraysymptoms.get(t).equals("jaundice") || arraysymptoms.get(t).equals("flu") || arraysymptoms.get(t).equals("diarrhea") || arraysymptoms.get(t).equals("cough") || arraysymptoms.get(t).equals("red eyes")) {
-                            highestcountdiseasesymptoms.add(arraysymptoms.get(t));
-                            possibleSymptomsCount++;
+                            //highestcountdiseasesymptoms.add(arraysymptoms.get(t));
+                            tempPossibleSymptomsCount++;
+                            Log.d("debug", arraysymptoms.get(t).toString() + " possibleSymptomsCount: " + String.valueOf(possibleSymptomsCount) + " tempPossibleSymptomsCount: " + String.valueOf(tempPossibleSymptomsCount));
                         }
                     }
+                    symptomsForPossibleDiseases.add(tempPossibleSymptomsCount);
+                    tempPossibleSymptomsCount = 0;
                 }
             }
         }
+        for (int i = 0; i < symptomsForPossibleDiseases.size(); i++) {
+            if (symptomsForPossibleDiseases.get(i) < possibleSymptomsCount)
+                possibleSymptomsCount = symptomsForPossibleDiseases.get(i);
+        }
+
         Chatstats cs= new Chatstats(highestcountdiseasearray, possiblediseases.size(), highestcount, possibleSymptomsCount);
         return cs;
         //printriskLevel(highestcountdiseasearray, possiblediseases.size(), highestcount, possibleSymptomsCount);
